454cdf150dd84d94459850200d9b84088182179b
Chevalier <0563531@students.dadeschools.net>
2018-04-12 12:07:56 -0400

    started

---
+++ logic.js
@@ -0,0 +1,17 @@
+var panel = $(" ");
+var countStartNumber = 30;
+
+// Question set
+var question = [{
+	question: "The atomic number of an atom is the number of ________ in the atom's nucleus." 
+	asnwers: ["electrons","neutrons","protons","valence electrons"]
+	correctAnswer: "Protons",
+	image: "assets/Atom-image-for-question-on-atomic-number.jpg"
+},{
+	question: "In a salt water solution, what substance is considered the solvent?" 
+	asnwers: ["solute","solvent","water","salt"]
+	correctAnswer: "Protons",
+	image: "assets/Atom-image-for-question-on-atomic-number.jpg"
+}]
+
+
